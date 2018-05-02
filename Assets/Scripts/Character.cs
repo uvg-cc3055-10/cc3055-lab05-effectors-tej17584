1d8011f14e4295d53f0f7786712bb933fc7a194e
Alhvi Balcarcel <abalcarcel@gmail.com>
2018-05-01 22:47:54 -0600

    repositorio base

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour {
+
+
+    Rigidbody2D rb2d;
+    SpriteRenderer sr;
+    Animator anim;
+    private float speed = 5f;
+    private float jumpForce = 250f;
+    private bool facingRight = true;
+ 
+
+
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+	
+
+	void Update () {
+        float move = Input.GetAxis("Horizontal");
+        if (move != 0) {
+            rb2d.transform.Translate(new Vector3(1, 0, 0) * move * speed * Time.deltaTime);
+            facingRight = move > 0;
+        }
+
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        sr.flipX = !facingRight;
+
+        if (Input.GetButtonDown("Jump")) {
+            rb2d.AddForce(Vector2.up*jumpForce);
+        }
+	}
+}
